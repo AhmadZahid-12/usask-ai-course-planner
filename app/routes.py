b881eb33f50aa5c55bc2ce444e96f7ce254ad5a2
Ahmad Zahid <ahmadzahid@Mac.gateway.sasktel.net>
2025-09-09 22:20:01 -0600

    Added comments and an extra function '/about' which routes to serve about.html, to present the next template.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
 from flask import Blueprint, render_template
 
+# Defined to access the main 'Blueprint' function
 bp = Blueprint("main", __name__)
+
+
+# Observing the site address and taking actions accordingly
+# and expanding the site templates accordingly
 
 @bp.route("/")
 def home():
+    # If the site address has a dash
+    # it returns the template to the 'index.html' template
     return render_template("index.html")
+
+@bp.route("/about")
+def about():
+    # If the site address has a dash with an 'about'
+    # it returns the template to the 'about.html' template
+    return render_template("about.html")
